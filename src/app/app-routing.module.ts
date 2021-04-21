661c59ae65d38b23fff56ce87768360c014c8a3d
Anshul Kumar Verma <anshul.k@clixlogix.net>
2021-04-21 15:31:18 +0530

    change route for bye compopnent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HelloComponent } from './hello/hello.component';
 
 const routes: Routes = [
   {path: '', component: HelloComponent},
-  {path: '', component: ByeComponent},
+  {path: 'bye', component: ByeComponent},
 ];
 
 @NgModule({
